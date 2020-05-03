f45bc32a9ef55af4a19d7d37e5b4c877d3ac7bca
CARICOM12 <0190a@ukr.net>
2020-05-03 15:04:22 +0300

    Add files via upload

---
+++ ForLoop1/ForLoop1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ForLoop1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int count;
+            for (count = 0; count < 5; count = count + 1)
+                Console.WriteLine("This is count: " + count);
+            Console.WriteLine("Done!");
+        }
+    }
+}
+
